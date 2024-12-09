68471f92756f27ba172152707b7a534294fdb0f1
fridoustin <fridolinaustin16@gmail.com>
2024-12-09 22:30:29 +0800

    feat: make home page

---
+++ frontend/src/app/home/page.tsx
@@ -0,0 +1,13 @@
+import { Navbar } from '@/components/Navbar/navbar'
+import React from 'react'
+
+const Home = () => {
+    return (
+        <div>
+            < Navbar/>
+            <div>Home</div>
+        </div>
+    )
+}
+
+export default Home

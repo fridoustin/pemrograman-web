917b4fde0fe8fc4f2e2d6c66c39fee6e1d687bec
fridoustin <fridolinaustin16@gmail.com>
2024-12-09 22:27:49 +0800

    feat: make /user route

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -14,3 +14,7 @@ Route::apiResource('posts', PostController::class);
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
 Route::post('/logout', [AuthController::class, 'logout'])->middleware('auth:sanctum');;
+
+Route::middleware('auth:sanctum')->get('/current-user', function (Request $request) {
+    return response()->json($request->user());
+});

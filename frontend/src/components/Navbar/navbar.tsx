be07029fabb79a3ef4a2f3158e7b0fb0571b0151
fridoustin <fridolinaustin16@gmail.com>
2024-12-10 13:05:03 +0800

    style: delete drawer on navbar

--- frontend/src/components/Navbar/navbar.tsx
+++ frontend/src/components/Navbar/navbar.tsx
@@ -62,58 +62,23 @@ export const Navbar = () => {
     };
 
     return (
-        <div className="drawer z-20">
-            <input id="my-drawer-3" type="checkbox" className="drawer-toggle" />
-            <div className="drawer-content flex flex-col">
-                <div className="navbar bg-sky-300 text-white fixed top-0 w-full gap-2 px-8 py-4 md:px-28">
+        <div className="z-20">
+                <div className="navbar bg-sky-300 text-white fixed top-0 w-full gap-2 px-6 py-3 md:px-16">
                     <div className="navbar-start">
-                        <label
-                        htmlFor="my-drawer-3"
-                        aria-label="open sidebar"
-                        className="lg:hidden btn btn-ghost"
-                        >
-                        <svg
-                            xmlns="http://www.w3.org/2000/svg"
-                            className="h-5 w-5"
-                            fill="none"
-                            viewBox="0 0 24 24"
-                            stroke="currentColor"
-                        >
-                            <path
-                            strokeLinecap="round"
-                            strokeLinejoin="round"
-                            strokeWidth="2"
-                            d="M4 6h16M4 12h8m-8 6h16"
-                            />
-                        </svg>
-                        </label>
                         <div className="text-xl font-bold text-black">
                             <p>Codemy Notes</p>
                         </div>
                     </div>
-                    <div className="navbar-center hidden lg:flex">
-                        <ul className="menu menu-horizontal px-1">
-                        <li><a href="/">Home</a></li>
-                        <li><a href="/notes">Notes</a></li>
-                        </ul>
-                    </div>
-                    <div className="navbar-end pr-8">
-                    {isLoggedIn ? (
-                            <>
-                                <span className="text-black font-bold mr-4 ">{userName}</span>
-                                <button
-                                    className="btn bg-red-500 border-none text-white hover:bg-red-700"
-                                    onClick={() => setShowLogoutPopup(true)} 
-                                >
-                                    Logout
-                                </button>
-                            </>
-                        ) : (
-                            <a className="btn bg-slate-200 text-black border-none shadow-md hover:bg-slate-400" href="/auth/signin">Login</a>
-                        )}
+                    <div className="navbar-end flex items-center">
+                        <span className="text-black font-bold mr-4 ">{userName}</span>
+                        <button
+                            className="btn bg-red-500 border-none text-white hover:bg-red-700 px-4 py-2"
+                            onClick={() => setShowLogoutPopup(true)} 
+                        >
+                            Logout
+                        </button>
                     </div>
                 </div>
-            </div>
 
             {showLogoutPopup && (
                 <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50 z-50">
@@ -140,21 +105,6 @@ export const Navbar = () => {
                     </div>
                 </div>
             )}
-
-            <div className="drawer-side">
-                <label
-                htmlFor="my-drawer-3"
-                aria-label="close sidebar"
-                className="drawer-overlay"
-                ></label>
-                <ul className="menu min-h-full w-60 bg-black/[0.9] pt-14 text-white font-bold">
-                    <li><a href="/">Home</a></li>
-                    <li><a href="/about">About</a></li>
-                    <li><a href="/program">Program</a></li>
-                    <li><a href="/event">Event</a></li>
-                    <li><a href="/membership">Membership</a></li>
-                </ul>
-            </div>
         </div>
     )
 }
